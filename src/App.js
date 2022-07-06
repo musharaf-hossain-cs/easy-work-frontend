b6e1ccd601bb7030ac9901575fe04d4ccef2df37
Md. Musharaf Hossain <mdmusharaf8071@gmail.com>
2022-07-07 00:45:19 +0600

    Material UI and icons-material added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from './logo.svg';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import { ButtonGroup } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
+import Stack from '@mui/material/Stack';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
+
 
 function App() {
-  return (
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
+  return (    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,6 +32,20 @@ function App() {
         >
           Learn React
         </a>
+        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+
+        <Box sx={{ width: 200 }}>
+          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            <VolumeDown />
+            <Slider aria-label="Volume" value={value} onChange={handleChange} />
+            <VolumeUp />
+          </Stack>
+          <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+        </Box>
       </header>
     </div>
   );
