11236eda0cc64ae22abc2dd068039fb367908fff
Md. Musharaf Hossain <mdmusharaf8071@gmail.com>
2022-07-11 00:39:31 +0600

    Eslint problem solved

--- src/App.js
+++ src/App.js
@@ -9,46 +9,40 @@ import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-
 function App() {
-  const [value, setValue] = React.useState(30);
+ const [value, setValue] = React.useState(30);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  
-  return (    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ButtonGroup variant="contained" aria-label="outlined primary button group">
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup>
+ const handleChange = (event, newValue) => {
+  setValue(newValue);
+ };
 
-        <Box sx={{ width: 200 }}>
-          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-            <VolumeDown />
-            <Slider aria-label="Volume" value={value} onChange={handleChange} />
-            <VolumeUp />
-          </Stack>
-          <Slider disabled defaultValue={30} aria-label="Disabled slider" />
-        </Box>
-      </header>
-    </div>
-  );
+ return (
+  <div className="App">
+   <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+     Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+     Learn React
+    </a>
+    <ButtonGroup variant="contained" aria-label="outlined primary button group">
+     <Button>One</Button>
+     <Button>Two</Button>
+     <Button>Three</Button>
+    </ButtonGroup>
+
+    <Box sx={{ width: 200 }}>
+     <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+      <VolumeDown />
+      <Slider aria-label="Volume" value={value} onChange={handleChange} />
+      <VolumeUp />
+     </Stack>
+     <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+    </Box>
+   </header>
+  </div>
+ );
 }
 
 export default App;
