3c0b26d72b061f19259480da409f22f5ae6fae0d
Jayanta Sadhu <jayantasadhu4557@gmail.com>
2022-07-16 23:00:06 +0600

    fetch function done and working

--- src/actions/Fetch.js
+++ src/actions/Fetch.js
@@ -1,10 +1,10 @@
 const BACKEND_URL = 'http://localhost:8000';
 
 // eslint-disable-next-line no-unused-vars, camelcase
-function fetchBackendJSON(url, method, data_dict) {
+async function fetchBackendJSON(url, method, data_dict) {
  // eslint-disable-next-line camelcase
  const backend_url = `${BACKEND_URL}/${url}/`;
- fetch(backend_url, {
+ const response = await fetch(backend_url, {
   method,
   mode: 'cors',
   cache: 'no-cache',
@@ -14,14 +14,19 @@ function fetchBackendJSON(url, method, data_dict) {
   redirect: 'follow',
   referrerPolicy: 'no-referrer',
   body: JSON.stringify(data_dict),
- })
-  .then((res) => res.json())
-  .then((data) => {
-   console.log(data);
-   // if (data.success === true) {
-   //     return data;
-   // }
-
-   return data;
-  });
+ });
+ const data = await response.json();
+ return data;
 }
+
+export default fetchBackendJSON;
+
+// .then((res) => res.json())
+//   .then((data) => {
+//    console.log(data);
+//    // if (data.success === true) {
+//    //     return data;
+//    // }
+
+//    return data;
+//   });
