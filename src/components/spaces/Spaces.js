befdff50f201f0345cc734e96ebc1b7c414ae262
Jayanta Sadhu <jayantasadhu4557@gmail.com>
2022-07-17 10:30:49 +0600

    space viewing done

--- src/components/spaces/Spaces.js
+++ src/components/spaces/Spaces.js
@@ -1,31 +1,76 @@
-import React, { useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import React, { useEffect, useState } from 'react';
+
 import fetchBackendJSON from '../../actions/Fetch';
 
 // const bull = (
-//     <Box
-//         component="span"
-//         sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-//     >
-//         •
-//     </Box>
+//  <Box component="span" sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }} />
 // );
 
-const myExample = () => {
- const myArray = [];
- for (let i = 0; i < 5; i += 1) {
-  myArray.push(<h1>Hello</h1>);
- }
- return myArray;
-};
-
 function Spaces() {
+ const [cards, setCards] = useState([]);
+
+ // eslint-disable-next-line prettier/prettier, prefer-const
+ let tempCards = [];
+
  useEffect(() => {
-  fetchBackendJSON('taskmgmt/gettaskslist', 'POST', { project_id: 4 }).then((data) => {
-   console.log(data);
-  });
+  // eslint-disable-next-line prettier/prettier
+    let fetchedData;
+  async function fetchData() {
+   fetchedData = await fetchBackendJSON('taskmgmt/getprojects', 'POST', {});
+
+   fetchedData.project_list.forEach((project) => {
+    console.log(project);
+    tempCards.push(
+     <Card sx={{ minWidth: 275 }} key={project.id}>
+      <CardContent>
+       <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+        {project.title}
+       </Typography>
+       <Typography variant="h5" component="div">
+        {project.description}
+       </Typography>
+       <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Days Remaining: {project.remaining_time}
+       </Typography>
+       <Typography variant="body2">Starting Date: {project.start_date}</Typography>
+      </CardContent>
+      <CardActions>
+       <Button size="small">Go to Space</Button>
+      </CardActions>
+     </Card>
+    );
+   });
+   setCards(tempCards);
+   tempCards = [];
+  }
+  fetchData();
+  console.log(cards.length);
  }, []);
 
- return <div>{myExample()}</div>;
+ return (
+  <div>
+   <Typography variant="h2" component="div" gutterBottom>
+    Spaces
+   </Typography>
+   <div
+    style={{
+     width: '100%',
+     display: 'flex',
+     justifyContent: 'space-between',
+     alignItems: 'center',
+    }}
+   >
+    {cards.map((proj) => (
+     <div className="cardDiv">{proj}</div>
+    ))}
+   </div>
+  </div>
+ );
 }
 
 export default Spaces;
