4a555d72931393c80b4fbf4a18d8d77774e7c783
Tanberul Islam <71716273+Ruhan17@users.noreply.github.com>
2022-07-17 10:25:15 +0600

    Showing Backend Data in gantt chart

--- src/components/GanttChart.js
+++ src/components/GanttChart.js
@@ -1,27 +1,77 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Gantt, {
     Column,
     Dependencies,
     Editing,
-    Item,
-    ResourceAssignments,
-    Resources,
-    Tasks,
+    Item, Tasks,
     Toolbar,
     Validation
 } from 'devextreme-react/gantt';
 
-import { dependencies, resourceAssignments, resources, tasks } from './data';
+import fetchBackendJSON from '../actions/Fetch';
 
-function GanttChart() {
+
+// import { dependencies, resourceAssignments, resources, tasks } from './data';
+
+
+export default function GanttChart() {
+    const [tasks, setTasks] = useState([]);
+    const [dependencies, setDependencies] = useState([]);
+    useEffect(() => {
+        // eslint-disable-next-line prettier/prettier
+        let fetchedData;
+        async function fetchData() {
+         fetchedData = await fetchBackendJSON('taskmgmt/getproject_tasks', 'POST', { project_id: 4 });
+         console.log('In space');
+         console.log(fetchedData);
+         let temp = [];
+         fetchedData.task_list.forEach((task) => {
+            temp.push({
+                id: task.id,
+                parentId: task.parent_id,
+                title: task.title,
+                start: task.start,
+                end: task.end,
+                progress: 0,
+            });
+         });
+         setTasks(temp);
+         temp = [];
+         fetchedData.dependency_list.forEach((dependency) => {
+            setDependencies([
+                ...dependencies,
+                {
+                    id: dependency.id,
+                    predecessorId: dependency.predecessor_id,
+                    successorId: dependency.successor_id,
+                    type: 0,
+                }
+
+            ]);
+         });
+        //  fetchedData.task_list.forEach((task) => {
+        //   tempTasks.push({
+        //    taskid: task.id,
+        //    title: task.title,
+        //    priority: task.priority,
+        //    dueDate: new Date(task.end),
+        //    status: task.status,
+        //   });
+        //  });
+        //  setTasks(tempTasks);
+        //  tempTasks = [];
+        }
+        fetchData();
+       }, []);
+    //    console.log(tasks.length());
  return (
   <Gantt taskListWidth={500} scaleType="weeks" height={800} width="1550" >
    <Tasks dataSource={tasks} />
    <Dependencies dataSource={dependencies} />
-   <Resources dataSource={resources} />
-   <ResourceAssignments dataSource={resourceAssignments} />
+   {/* <Resources dataSource={resources} />
+   <ResourceAssignments dataSource={resourceAssignments} /> */}
 
    <Toolbar>
     <Item name="undo" />
@@ -46,5 +96,3 @@ function GanttChart() {
   </Gantt>
  );
 }
-
-export default GanttChart;
