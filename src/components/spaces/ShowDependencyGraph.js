89d02b5ba79a5d95154021f3e053b59316b9686d
Md. Musharaf Hossain <mdmusharaf8071@gmail.com>
2022-08-26 02:19:53 +0600

    Dependency Graph completed with backend data

--- src/components/spaces/ShowDependencyGraph.js
+++ src/components/spaces/ShowDependencyGraph.js
@@ -2,22 +2,20 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import fetchBackendJSON from '../../actions/Fetch';
-import DependencyGraph from './DependencyGraph';
+import DependencyGraph from '../views/DependencyGraph';
 
-const data = {
- // eslint-disable-next-line global-require
- image: require('../../assets/Critical-Path.png'),
- title: 'Image Title',
-};
+// const data = {
+//  // eslint-disable-next-line global-require
+//  image: require('../../assets/Critical-Path.png'),
+//  title: 'Image Title',
+// };
 
 const rows = [];
 
 function ShowDependencyGraph() {
  const { spaceid } = useParams();
  let { taskid } = useParams();
- const [dependencyDetails, setDependencyDetails] = useState(data);
- const [rowsPerPage, setRowsPerPage] = useState(10);
- const [page, setPage] = useState(0);
+ const [imgData, setImgData] = useState({ image: null, title: 'title', map: [] });
 
  if (taskid === undefined) {
   taskid = -1;
@@ -32,23 +30,25 @@ function ShowDependencyGraph() {
     task_id: taskid,
    });
    console.log(fetchedData);
-   setDependencyDetails(() => {
-    const newDep = JSON.parse(JSON.stringify(fetchedData.data));
-    return newDep;
-   });
+   if (fetchedData.success) {
+    console.log('DependencyGraph Data fetch successed');
+    setImgData({
+     image: fetchedData.data.image_name,
+     title: fetchedData.data.title,
+     map: fetchedData.data.task_map,
+    });
+   } else {
+    console.log('DependencyGraph Data fetch failed');
+   }
   }
   fetchData();
  }, []);
 
- const handleChangeRowsPerPage = (event) => {
-  setRowsPerPage(+event.target.value);
-  setPage(0);
- };
-
  return (
   <div>
-   <h3 className="alignCenter">Dependency Graph</h3>
-   <DependencyGraph spaceid={spaceid} taskid={taskid} rows={rows} imgData={data} />
+   <h3 className="alignCenter">Dependency Graph: {imgData.title}</h3>
+   <DependencyGraph spaceid={spaceid} taskid={taskid} rows={rows} imgData={imgData} />
+   <hr />
   </div>
  );
 }
