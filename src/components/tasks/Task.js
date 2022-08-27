0cd0c174633b157f44f2e432f7966bc79fe6bbd0
Tanberul Islam <71716273+Ruhan17@users.noreply.github.com>
2022-08-27 20:15:02 +0600

    User Commenting Done

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -1,22 +1,31 @@
+/* eslint-disable prefer-const */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { useNavigate, useParams } from 'react-router-dom';
 import fetchBackendJSON from '../../actions/Fetch';
-// import styles from '../../styles/TaskDetail.module.css';
-// import TaskDetail from './TaskDetail';
 import UserList from '../users/UserList';
 import TaskDetailList from './TaskDetailList';
 import TasksInTable from './TasksInTable';
 
+import styles from '../../styles/TaskDetail.module.css';
+
 function Task() {
  const { spaceid, taskid } = useParams();
  const navigate = useNavigate();
+ const userid = 2;
  const [taskDetails, setTaskDetails] = useState([]);
  const [tasks, setTasks] = useState([]);
  const [users, setUsers] = useState([]);
  const [toTask, setToTask] = useState(0);
+ const [comment, setComment] = useState('');
+ const [comments, setComments] = useState([]);
+ const [name, setName] = useState('');
  // eslint-disable-next-line prefer-const
  let tempTasks = [];
+ let tempComments = [];
  // eslint-disable-next-line prefer-const
 
  useEffect(() => {
@@ -90,6 +99,27 @@ function Task() {
   }
   fetchData();
  }, []);
+ useEffect(() => {
+  let fetchedData;
+  async function fetchData() {
+   fetchedData = await fetchBackendJSON('project/getcomments', 'POST', { task_id: taskid });
+   console.log(fetchedData);
+   fetchedData.comments_list.forEach((cmnt) => {
+    console.log('Date');
+    let commentTime = cmnt.comment.comment_time.split('T')[0];
+    commentTime += ` ${cmnt.comment.comment_time.split('T')[1].split('.')[0]}`;
+    console.log(commentTime);
+    tempComments.push({
+     commenterName: cmnt.user,
+     comment: cmnt.comment.comment,
+     commentTime,
+    });
+   });
+   setComments(tempComments);
+   tempComments = [];
+  }
+  fetchData();
+ }, []);
 
  const deleteTask = () => {
   async function sendData() {
@@ -100,40 +130,102 @@ function Task() {
   sendData();
  };
 
+ const submitComment = (e) => {
+  e.preventDefault();
+  console.log('Submitted');
+  const data = {
+   task: taskid,
+   user: userid,
+   comment,
+  };
+  console.log(data);
+  async function sendData() {
+   const res = await fetchBackendJSON('project/addcomments', 'POST', data);
+   console.log(res);
+   setComment('');
+   window.location.reload(true);
+   // navigate(`/spaces/${spaceid}/tasks/${taskid}/`);
+  }
+  sendData();
+ };
+
  return (
-  <div className="mycontainer container">
-   <TaskDetailList spaceid={spaceid} taskid={taskid} taskDetailList={taskDetails} />
-   {/* <TaskDetail spaceid={spaceid} taskid={taskid} taskDetails={taskDetails} /> */}
-   <Button
-    variant="primary"
-    style={{ margin: '5px' }}
-    onClick={() => navigate(`/spaces/${spaceid}/tasks/${taskid}/edit-task`, { replace: false })}
-   >
-    Edit Task
-   </Button>
-   <Button
-    variant="primary"
-    style={{ margin: '5px' }}
-    onClick={() => navigate(`/spaces/${spaceid}/tasks/${taskid}/new-task`, { replace: false })}
-   >
-    Add Subtask
-   </Button>
-   <Button
-    variant="primary"
-    style={{ margin: '5px' }}
-    onClick={() => navigate(`/spaces/${spaceid}/tasks/${taskid}/assign-member`, { replace: false })}
-   >
-    Assign Members
-   </Button>
-   <Button variant="danger" style={{ marginLeft: '800px' }} onClick={deleteTask}>
-    Delete Task
-   </Button>
-   <hr />
-   <h3>All SubTasks</h3>
-   <TasksInTable tasks={tasks} rowPerPage={3} toTaskSet={setToTask} />
-   <hr />
-   <h3>User List</h3>
-   <UserList users={users} rowPerPage={5} />
+  <div>
+   <div className={styles.left}>
+    <TaskDetailList spaceid={spaceid} taskid={taskid} taskDetailList={taskDetails} />
+    {/* <TaskDetail spaceid={spaceid} taskid={taskid} taskDetails={taskDetails} /> */}
+    <Button
+     variant="primary"
+     style={{ margin: '5px' }}
+     onClick={() => navigate(`/spaces/${spaceid}/tasks/${taskid}/edit-task`, { replace: false })}
+    >
+     Edit Task
+    </Button>
+    <Button
+     variant="primary"
+     style={{ margin: '5px' }}
+     onClick={() => navigate(`/spaces/${spaceid}/tasks/${taskid}/new-task`, { replace: false })}
+    >
+     Add Subtask
+    </Button>
+    <Button
+     variant="primary"
+     style={{ margin: '5px' }}
+     onClick={() =>
+      navigate(`/spaces/${spaceid}/tasks/${taskid}/assign-member`, { replace: false })
+     }
+    >
+     Assign Members
+    </Button>
+    <Button variant="danger" style={{ marginLeft: '500px' }} onClick={deleteTask}>
+     Delete Task
+    </Button>
+    <hr />
+    <h3>All SubTasks</h3>
+    <TasksInTable tasks={tasks} rowPerPage={3} toTaskSet={setToTask} />
+    <hr />
+    <h3>User List</h3>
+    <UserList users={users} rowPerPage={5} />
+   </div>
+   <div className={styles.vertical} />
+   <div className={styles.right}>
+    <h2>Comments</h2>
+    <hr />
+    <div>
+     {comments.map((val, key) => (
+      <>
+       <span key={key}>
+        {' '}
+        <b>{val.commenterName}</b>
+       </span>{' '}
+       <br />
+       <span key={key}> {val.comment}</span> <br />
+       <span key={key}>
+        {' '}
+        <i> {val.commentTime}</i>
+       </span>{' '}
+       <br />
+       <hr />
+      </>
+     ))}
+    </div>
+    <h3>Add Comment</h3>
+    <Form.Group className="mb-3 col-3" controlId="formTaskDescription">
+     <Form.Control
+      as="textarea"
+      rows={4}
+      value={comment}
+      placeholder="Put your Comment Here"
+      onChange={(e) => setComment(e.target.value)}
+      onKeyPress={(e) => {
+       if (e.key === 'Enter') e.preventDefault();
+      }}
+     />
+    </Form.Group>
+    <Button variant="primary" style={{ margin: '5px' }} onClick={submitComment}>
+     Submit Comment
+    </Button>
+   </div>
   </div>
  );
 }
--- src/components/tasks/TaskDetail.js
+++ src/components/tasks/TaskDetail.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import styles from '../../styles/TaskDetail.module.css';
 
 function TaskDetail({ taskid, spaceid, taskDetails }) {
@@ -9,52 +10,60 @@ function TaskDetail({ taskid, spaceid, taskDetails }) {
   [date.getFullYear(), date.getMonth() + 1, date.getDate() + 1].join('-');
 
  return (
-  <div>
-   <div>
-    <h2>{taskDetails.title}</h2>
-    <h6>Description: {taskDetails.description}</h6>
+  <div className={styles.splitScreen}>
+   <div className={styles.topPane}>
+    <h1>Top Pane</h1>
    </div>
-   <hr />
-   <div className="row">
-    <div className="col-6">
-     <b>Properties</b>
-     <div className={styles.paddingLeft20px}>
-      Priority: {taskDetails.priority} <br />
-      Status: {taskDetails.status}
-     </div>
-    </div>
-    <div className="col-6">
-     <b>Dates</b>
-     <div className={styles.paddingLeft20px}>
-      Create Date: {formatDate(taskDetails.createDate)} <br />
-      Start Date: {formatDate(taskDetails.startDate)} <br />
-      End Date: {formatDate(taskDetails.endDate)}
-     </div>
-    </div>
-
-    <div className="col-6">
-     <b>All Attachments:</b> <br />
-     <div className={styles.paddingLeft20px}>
-      {taskDetails.attachments.map((val, key) => (
-       <>
-        <span key={key}>{val}</span> <br />
-       </>
-      ))}
-     </div>
-    </div>
-
-    <div className="col-6">
-     <b>All Comments:</b> <br />
-     <div className={styles.paddingLeft20px}>
-      {taskDetails.comments.map((val, key) => (
-       <>
-        <span key={key}>{val}</span> <br />
-       </>
-      ))}
-     </div>
-    </div>
+   <div className={styles.bottomPane}>
+    <h1>Bottom Pane</h1>
    </div>
   </div>
+  //   <div>
+  //    <div>
+  //     <h2>{taskDetails.title}</h2>
+  //     <h6>Description: {taskDetails.description}</h6>
+  //    </div>
+  //    <hr />
+  //    <div className="row">
+  //     <div className="col-6">
+  //      <b>Properties</b>
+  //      <div className={styles.paddingLeft20px}>
+  //       Priority: {taskDetails.priority} <br />
+  //       Status: {taskDetails.status}
+  //      </div>
+  //     </div>
+  //     <div className="col-6">
+  //      <b>Dates</b>
+  //      <div className={styles.paddingLeft20px}>
+  //       Create Date: {formatDate(taskDetails.createDate)} <br />
+  //       Start Date: {formatDate(taskDetails.startDate)} <br />
+  //       End Date: {formatDate(taskDetails.endDate)}
+  //      </div>
+  //     </div>
+
+  //     <div className="col-6">
+  //      <b>All Attachments:</b> <br />
+  //      <div className={styles.paddingLeft20px}>
+  //       {taskDetails.attachments.map((val, key) => (
+  //        <>
+  //         <span key={key}>{val}</span> <br />
+  //        </>
+  //       ))}
+  //      </div>
+  //     </div>
+
+  //     <div className="col-6">
+  //      <b>All Comments:</b> <br />
+  //      <div className={styles.paddingLeft20px}>
+  //       {taskDetails.comments.map((val, key) => (
+  //        <>
+  //         <span key={key}>{val}</span> <br />
+  //        </>
+  //       ))}
+  //      </div>
+  //     </div>
+  //    </div>
+  //   </div>
  );
 }
 
